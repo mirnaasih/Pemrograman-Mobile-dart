33b3c9e8cca297d6ce122ce362600073e2d2a5a2
mirnaasih <mirna@student.undiksha.ac.id>
2026-09-19 21:23:16 +0800

    Create latihan-1.dart

---
+++ level-1/latihan-1.dart
@@ -0,0 +1,12 @@
+// Nama: Ni Komang Mirna Asih
+// NIM: 2415051018
+
+void main() {
+  print('Nama: Ni Komang Mirna Asih');
+  print('NIM: 2415051018');
+
+  String programStudi = 'Pendidikan Teknik Informatika';
+  String tujuan = 'membangun aplikasi mobile yang interaktif dan bermanfaat';
+
+  print('Halo! Saya dari program studi $programStudi dan saya mempelajari pemrograman mobile karena ingin $tujuan.');
+
